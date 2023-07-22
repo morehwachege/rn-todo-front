66881051c2152746c378a2c413301999f6b76e2c
Antony Gakuru <antony123muriithi@gmail.com>
2023-07-22 13:25:27 +0300

    add todo serializer, model, controller methods

--- Gemfile
+++ Gemfile
@@ -46,3 +46,5 @@ group :development do
   # gem "spring"
 end
 
+
+gem "active_model_serializers", "~> 0.10.13"
--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,2 +1,27 @@
 class TodosController < ApplicationController
+
+  def index
+    @todos = Todo.all
+    render json: @todos, status: :ok
+  end
+
+  def create
+    @todo = Todo.create!(todo_params)
+    render json: @todo, status: :created
+  end
+
+  def destroy
+    @todo = Todo.find(params[:id])
+    @todo.destroy
+    head: no_content
+  end
+  
+  private
+
+  def todo_params
+    params.require(:todo).permit(:title, :description, :completed)
+  end
 end
+
+
+
