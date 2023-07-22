07068a0b0b976a11028ebabff8960fd59d940485
Antony Gakuru <antony123muriithi@gmail.com>
2023-07-22 13:39:12 +0300

    add todo rescue methods

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,4 +1,6 @@
 class TodosController < ApplicationController
+rescue_from ActiveRecord::RecordNotFound, with: :not_found
+rescue_from ActiveRecord::RecordInvalid, with: :record_invalid
 
   def index
     @todos = Todo.all
@@ -10,17 +12,37 @@ class TodosController < ApplicationController
     render json: @todo, status: :created
   end
 
+  def show
+    @todo = Todo.find(params[:id])
+    render json: @todo, status: :ok
+  end
+
+  def update
+    @todo = Todo.find(params[:id])
+    @todo.update!(todo_params)
+    render json: @todo, status: :accepted
+  end
+
   def destroy
     @todo = Todo.find(params[:id])
     @todo.destroy
-    head: no_content
+    head :no_content
   end
-  
+
   private
 
   def todo_params
     params.require(:todo).permit(:title, :description, :completed)
   end
+
+  def not_found
+    render json: { error:  "Todo not found" }, status: :not_found
+  end
+
+  def record_invalid(invalid)
+    render json: { errors: invalid.record.errors }, status: :unprocessable_entity
+  end
+
 end
 
 
