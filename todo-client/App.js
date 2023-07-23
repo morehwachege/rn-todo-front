8b5737a4033a3122ec994e7529d78e9ff274edb4
Antony Gakuru <antony123muriithi@gmail.com>
2023-07-23 03:02:28 +0300

    add todo-client folder

---
+++ todo-client/App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+import TodoPage from './screens/TodoPage';
+import WelcomePage from './screens/WelcomePage';
+
+
+function App() {
+  const Stack = createStackNavigator();
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Welcome" component={WelcomePage} options={{title: 'Welcome', headerTitleAlign: 'center', headerShown: false}}/>
+
+        <Stack.Screen
+          name="TodoPage
+          "
+          component={TodoPage}
+          options={{ title: 'Todo', headerShown: false }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
